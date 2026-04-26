d5dd995eef1ec063a8672ccd5fae6c43af880038
aandroth <aandroth@gmail.com>
2026-04-26 05:45:10 -0700

    Updated hostname

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const GAME_STATE = Object.freeze({
  const serverOptions = {
     cert: CERT,
      key: PRIV,
-     host: "masktego.aquinsgreatgames"
+     hostname: "masktego.aquinsgreatgames"
 };
 
 const m_allowedOrigins = [
